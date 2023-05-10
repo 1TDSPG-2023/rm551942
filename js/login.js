af34de0727f0a80363a2429a4479f725b13b17b1
Dullinho_- <rm551942@fiap.com.br>
2023-05-10 09:24:21 -0300

    Loging.html criada

---
+++ js/login.js
@@ -0,0 +1,44 @@
+//Gerando um token com Math.
+let tokenGerado = Math.random().toString(16).substring(2);
+//DECLARANDO OBJETOS
+const usuario1 = {
+    nomeUsuario : "denden",
+    senhaUsuario: "12345",
+    gravaDados : true,
+    token : tokenGerado
+}
+
+tokenGerado = Math.random().toString(16).substring(2);
+//DECLARANDO OBJETOS
+const usuario2 = {
+    nomeUsuario : "gege",
+    senhaUsuario: "12345",
+    gravaDados : true,
+    token : tokenGerado
+}
+
+let listaDeUsuarios = [];
+listaDeUsuarios.push(usuario1);
+listaDeUsuarios.push(usuario2);
+
+addEventListener("click", (evt)=>{
+
+    if(evt.target.id == "btnSubmit"){
+        
+        const inputUser = document.querySelector("#idUser");
+        const inputPass = document.querySelector("#idPass");
+        
+        listaDeUsuarios.forEach((usuario)=>{
+
+            if(inputUser.value == usuario.nomeUsuario && inputPass.value == usuario.senhaUsuario){
+                console.log("VALIDADO");
+            }else{
+                console.log("NÃO VALIDADO");
+            }
+
+        });
+    }
+
+
+    
+});
